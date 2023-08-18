ece8e6801dd0aaa4f9f97b15b698ad87e8f22a40
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-08-18 16:30:48 +0300

    White prompt() algorithm to set lengths of sides of <main>

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 let body = document.querySelector('body'),
-    main = document.createElement('main');
+    main = document.createElement('main'),
+    sideLength = 0,
+    mainHight = main.style.height = '960px';
+    mainWidth = main.style.width = mainHight,
+    mainHeightDigit = '',
+    divsNumber = 0;
+    // divHeight = div.style.height;
+    // divWidth = div.style.width;
 
-body.appendChild(main);    
+body.appendChild(main);   
+
+sideLength = prompt('Type the length of a side you want:');
+sideLength = parseInt(sideLength);
+mainHeightDigit = mainHight.substring(0, 3);
+mainHeightDigit = parseInt(mainHeightDigit);
+divsNumber = sideLength * sideLength;
+
     
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
+for (let i = 0; i < divsNumber; i++) {
+
+    div = document.createElement('div');
+    div.style.height = mainHeightDigit / sideLength + 'px';
+    div.style.width = mainHeightDigit / sideLength + 'px';
     main.appendChild(div);
-}
+    
+};  
 
 let divs = document.querySelectorAll('div');
 
 divs.forEach((item) => {
+   
     item.addEventListener('mouseover', () => {
         item.classList.add('hovering');
-});
+    });
 });
