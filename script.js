8d6565e519393fb3e12e1a181fa7a36242ff2b6b
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-08-21 13:26:08 +0300

    Create toSetField() and refresh buttons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let body = document.querySelector('body'),
     main = document.createElement('main'),
+    button = document.createElement('button'),
+    button2 = document.createElement('button'),
     sideLength = 0,
     mainHight = main.style.height = '960px';
     mainWidth = main.style.width = mainHight,
@@ -8,7 +10,32 @@ let body = document.querySelector('body'),
     // divHeight = div.style.height;
     // divWidth = div.style.width;
 
-body.appendChild(main);   
+    body.appendChild(button);
+    button.textContent += 'Set length of a side';
+    body.appendChild(main);
+    
+    body.appendChild(button2);
+    button2.textContent += 'Refresh';
+    button2.classList.add('button2');
+    body.appendChild(main); 
+
+button2.addEventListener('click', () => {
+    location.reload();
+})
+
+button.addEventListener('click', () => {
+    if (sideLength == 0) {
+        toSetField();
+    } else {
+        location.reload(); 
+    }
+});
+
+
+
+
+
+function toSetField(sideLength) {
 
 sideLength = prompt('Type the length of a side you want:');
 sideLength = parseInt(sideLength);
@@ -16,15 +43,13 @@ mainHeightDigit = mainHight.substring(0, 3);
 mainHeightDigit = parseInt(mainHeightDigit);
 divsNumber = sideLength * sideLength;
 
-    
 for (let i = 0; i < divsNumber; i++) {
-
     div = document.createElement('div');
     div.style.height = mainHeightDigit / sideLength + 'px';
     div.style.width = mainHeightDigit / sideLength + 'px';
     main.appendChild(div);
     
-};  
+}; 
 
 let divs = document.querySelectorAll('div');
 
@@ -34,3 +59,17 @@ divs.forEach((item) => {
         item.classList.add('hovering');
     });
 });
+
+};
+
+
+    
+// sideLength = parseInt(sideLength);
+// mainHeightDigit = mainHight.substring(0, 3);
+// mainHeightDigit = parseInt(mainHeightDigit);
+// divsNumber = sideLength * sideLength;
+
+    
+ 
+
+
